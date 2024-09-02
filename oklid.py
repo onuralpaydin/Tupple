d10d5cfe70454fdc9d1aaa6b61cd9589a622f688
Onur Alp Aydın <o.a.aydin17@gmail.com>
2024-09-02 16:15:59 +0300

    oklid distance calculate

---
+++ oklid.py
@@ -0,0 +1,22 @@
+import math
+
+points = [(1, 2), (3, 4), (5, 6), (7, 8)]
+
+def oklidDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distance
+
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = oklidDistance(points[i], points[j])
+        distances.append(distance)
+
+# Minimum mesafenin bulunması
+if distances:
+    min_distance = min(distances)
+    print("Minimum mesafe:", min_distance)
+else:
+    print("Mesafe hesaplanacak nokta yok.")
